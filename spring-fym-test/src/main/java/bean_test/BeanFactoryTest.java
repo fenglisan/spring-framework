34ae98114f75f03bfb5627dedc37c6962bdf6dd6
fenglisan99 <fenglisan99@163.com>
2023-05-13 23:20:03 +0800

    自测

---
+++ spring-fym-test/src/main/java/bean_test/BeanFactoryTest.java
@@ -0,0 +1,15 @@
+package bean_test;
+
+
+@SuppressWarnings("deprecation")
+public class BeanFactoryTest {
+
+
+	public static void main(String[] args) {
+
+
+	}
+
+
+
+}
---
+++ spring-fym-test/src/main/java/bean_test/MyTestBean.java
@@ -0,0 +1,13 @@
+package bean_test;
+
+public class MyTestBean {
+	private String testStr = "testStr";
+
+	public String getTestStr() {
+		return testStr;
+	}
+
+	public void setTestStr(String testStr) {
+		this.testStr = testStr;
+	}
+}
